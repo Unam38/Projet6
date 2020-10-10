1b514e24655466b6858fcc8e7dd0b435582a8d0d
Emmanuel Daubeuf <emmanueldaubeuf@gmail.com>
2020-10-10 08:52:50 +0200

    update création like dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -49,3 +49,59 @@ exports.getAllSauces = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+exports.likeSauce = (req, res, next) => {
+
+  switch (req.body.like) {
+    //cancel = 0
+    //check if the user had liked or disliked the sauce
+    //uptade the sauce, send message/error
+    case 0:
+      Sauces.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          if (sauce.usersLiked.find(user => user === req.body.userId)) {
+            Sauces.updateOne({ _id: req.params.id }, {
+              $inc: { likes: -1 },
+              $pull: { usersLiked: req.body.userId },
+              _id: req.params.id
+            })
+              .then(() => { res.status(201).json({ message: 'Ton avis a été pris en compte!' }); })
+              .catch((error) => { res.status(400).json({ error: error }); });
+
+          } if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+            Sauces.updateOne({ _id: req.params.id }, {
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: req.body.userId },
+              _id: req.params.id
+            })
+              .then(() => { res.status(201).json({ message: 'ok...' }); })
+              .catch((error) => { res.status(400).json({ error: error }); });
+          }
+        })
+        .catch((error) => { res.status(404).json({ error: error }); });
+      break;
+    //likes = 1
+    //uptade the sauce, send message/error
+    case 1:
+      Sauces.updateOne({ _id: req.params.id }, {
+        $inc: { likes: 1 },
+        $push: { usersLiked: req.body.userId },
+        _id: req.params.id
+      })
+        .then(() => { res.status(201).json({ message: 'Like added!' }); })
+        .catch((error) => { res.status(400).json({ error: error }); });
+      break;
+    //likes = -1
+    //uptade the sauce, send message/error
+    case -1:
+      Sauces.updateOne({ _id: req.params.id }, {
+        $inc: { dislikes: 1 },
+        $push: { usersDisliked: req.body.userId },
+        _id: req.params.id
+      })
+        .then(() => { res.status(201).json({ message: 'Ok... it\'\s your right...' }) })
+        .catch((error) => { res.status(400).json({ error: error }) });
+      break;
+    default:
+      console.error('Bad request')
+  }
+};
